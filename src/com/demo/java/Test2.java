6bbdeb8828e4a763bfaf9638f70f86105f952419
USER <USER@HP>
2020-02-12 14:12:02 +0530

    Test 2 added

--- src/com/demo/java/Test2.java
+++ src/com/demo/java/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("New TEst file added");
 		System.out.println("y is added");
+		System.out.println("Hello");
 	}
 
 }
